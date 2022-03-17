ca02ca713efde720e72a20ddc44d919b3c518ec6
GarmaNazareno <garmatorresnazareno@gmail.com>
2022-03-16 21:04:31 -0300

    Updates

---
+++ precache-manifest.c80e55ee16bf42787591591c6e52cc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94341cc14efdefd29322496082032e71",
+    "url": "/basic-tours/index.html"
+  },
+  {
+    "revision": "631826923e712393d5e1",
+    "url": "/basic-tours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/basic-tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631826923e712393d5e1",
+    "url": "/basic-tours/static/js/main.ee867818.chunk.js"
+  },
+  {
+    "revision": "c22b99df51163016bea1",
+    "url": "/basic-tours/static/js/runtime-main.e5223601.js"
+  }
+]);
